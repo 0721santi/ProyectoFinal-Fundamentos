2e79be114ab2eea9051a8eb2340fe285e7b6de29
0721santi <san200526@gmail.com>
2023-05-19 13:02:45 -0500

    Lectura de archivo TXT con lista de palabras.

---
+++ main.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
+public class main{
+    static void creaArrayPalabras(String path, ArrayList<String> lista){
+        try {
+            File doc = new File(path+".txt");
+            Scanner readDoc = new Scanner(doc);       
+            while(readDoc.hasNextLine()){
+                lista.add(readDoc.nextLine());
+            }
+        } catch (Exception e) {
+            System.out.println("No existe un archivo con nombre "+path+".");
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        Scanner myScan = new Scanner(System.in);
+        System.out.print("Ingrese nombre del archivo: ");
+        String path = myScan.nextLine();
+        ArrayList<String> listaPalabras = new ArrayList<>();
+        creaArrayPalabras(path, listaPalabras);
+        for(int i=0;i<listaPalabras.size();i++){
+            System.out.println(listaPalabras.get(i));
+        }
+    }
+}
